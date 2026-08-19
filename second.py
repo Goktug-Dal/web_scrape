3f5f7bfa7aea7b8e8141b4a236e4da8fe41b5e96
GKT01 <goktugdal34@gmail.com>
2026-08-19 21:24:04 +0300

    .

--- second.py
+++ second.py
@@ -5,6 +5,12 @@ with open('home.html', 'r') as html_file:
     soup = BeautifulSoup(content,"lxml")
 
     courses_html_tags = soup.find_all("h5", attrs= {"class": "card-title"})
+    course_cards = soup.find_all("div", attrs={"class": "card"})
 
     for course in courses_html_tags:
         print(course.text + "\n")
+
+    for course in course_cards:
+        course_name = course.h5.text
+        course_price = course.a.text
+        print(course_name + " " + course_price)
